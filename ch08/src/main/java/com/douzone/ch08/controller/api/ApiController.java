f2ef3e2d76b35ffb8a2879968ebe557fcb0a229e
sysyo <youngiya77@gmail.com>
2021-11-11 13:37:15 +0900

    Ajax #2

--- ch08/src/main/java/com/douzone/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/com/douzone/ch08/controller/api/ApiController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.douzone.ch08.controller.vo.GuestBookVO;
+
 @Controller
 @RequestMapping("/api")
 public class ApiController {
@@ -13,4 +15,22 @@ public class ApiController {
 	public String text() {
 		return "Text 데이터";
 	}
+	
+	@ResponseBody
+	@RequestMapping("/html")
+	public String html() {
+		return "<h1>AJAX 연습</h1><p>HTML 데이터</p>";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/xml")
+	public Object xml() {
+		GuestBookVO vo = new GuestBookVO();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setMessage("호이~");
+		
+		return vo;
+	}
+	
 }

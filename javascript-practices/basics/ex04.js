cbd512653914a43517a27b1a21a70ade701c8249
sysyo <youngiya77@gmail.com>
2021-11-11 10:37:23 +0900

    1110 study

--- javascript-practices/basics/ex04.js
+++ javascript-practices/basics/ex04.js
@@ -21,7 +21,7 @@
 
 var i = 10;
 var f = function() {
-    var i = 20;
+    var i = 20; // 함수 밖으로 나가면 var i 값이 사라짐
     j = 100;
     console.log("i : " +  i); // 결과 : 20
     i = j -1;
@@ -32,13 +32,14 @@ var f = function() {
     let x = 1000000;
     const PI = 3.14;
     x = 100;
-    // 상수이기 때문에 에러
-    // PI = 0;
+    // PI = 0; // const 재할당 안되기 때문에 
+    // 변수선언에 기본적으로는 const 를 사용해야하고 재할당을 해야하는 경우에는 let을 사용하는 게 좋다
+    // 재할당 : const PI = 0; 아니고 PI =0; 
 }
 
 // error: 접근이 안됨
 // console.log(x);
 
-f();
-console.log("i : " + i); // 결과 : 10
+f(); // 안들고오면 안됨 - var 붙이면 지역변수 안붙이면 전역변수 // f(); 는 초기화 되는 것
+console.log("i : " + i); // 결과 : 10 // f();로 초기화 시켜주기 때문에  var i = 10;
 console.log("j : " + j); // 결과 : 100

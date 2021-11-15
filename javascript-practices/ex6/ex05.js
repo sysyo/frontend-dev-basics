89986c0a51c5e4fa7657e44f67be213d7864df5f
sysyo <youngiya77@gmail.com>
2021-11-15 15:54:20 +0900

    es6 #2

--- javascript-practices/ex6/ex05.js
+++ javascript-practices/ex6/ex05.js
@@ -30,4 +30,29 @@ numbers.forEach(function(n){
 });
 
 // ex2
+console.log("\n----------\n");
+numbers.forEach(n => process.stdout.write(`${n}:${(x => x * x)(n)}\t`));
 
+// ex3 -여러 행 함수
+console.log("\n----\n");
+[5, 3, 15, 1045, 43, 92].forEach(e => {
+    if(e % 5 == 0 ) {
+        process.stdout.write(`${e}\t`);
+    }
+});
+
+// ex4 - this를 어휘적으로 바인딩 (Lexical Bind)
+console.log("\n-------\n");
+const dooly = {
+    name: '둘리',
+    friends: ['또치', '마이콜', '도우너', '길동'],
+    printFriends: function() {
+        // this.friends.forEach(function(f){
+        //     console.log(`${this.name}의 친구 ${f}`);   
+        // }.bind(this));
+
+        this.friends.forEach(f => console.log(`${this.name}의 친구 ${f}`));
+    }
+}
+
+dooly.printFriends();
